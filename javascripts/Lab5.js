8e004fde0bbb1fc2100a57ff5685d0dc58610a2a
zjtxljy <zjtxljyus@gmail.com>
2017-04-26 18:47:23 -0400

    edit function handleTextureLoaded(texture)

--- javascripts/Lab5.js
+++ javascripts/Lab5.js
@@ -158,6 +158,15 @@ var shaderProgramSB;   // shader program for the sky box (environment cube)
 		sampleTexture.image.src = "images/texture.jpg";   
 	}
 
+
+	function handleTextureLoaded(texture) {
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		gl.bindTexture(gl.TEXTURE_2D, null);
+	}
+
     ///////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////
 
